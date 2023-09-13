0ed0bd7f3575b28bf28e9c51bd6a67d414b408b6
gogs <gogs@everis.com>
2023-09-13 07:25:35 +0000

    Commit changes to all files

--- src/test/java/com/dedalow/utils/Utils.java
+++ src/test/java/com/dedalow/utils/Utils.java
@@ -222,13 +222,13 @@ public class Utils {
      */
     public static ArrayList<String> getTestCases(String option, ArrayList<String> testCases) throws Exception {
         switch (option) {
-            case "fgt":
-            	testCases.add("com.dedalow.fgt.Test_TestCaseModel");
+            case "asd":
+            	testCases.add("com.dedalow.asd.Test_TestCaseModel");
 			
             break;
 			
             case "complete":
-                	testCases.add("com.dedalow.fgt.Test_TestCaseModel");
+                	testCases.add("com.dedalow.asd.Test_TestCaseModel");
 			
                 break;
             default:
